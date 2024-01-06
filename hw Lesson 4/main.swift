b40e144ba11a6c35218de13eae9de56b514276ba
bekturinho <ismanalievbektur4@gmail.com>
2024-01-05 23:10:05 -0800

    ds

--- hw Lesson 4/main.swift
+++ hw Lesson 4/main.swift
@@ -1,11 +1,54 @@
-//
-//  main.swift
-//  hw Lesson 4
-//
-//  Created by fortune cookie on 1/5/24.
-//
 
 import Foundation
 
-print("Hello, World!")
+
+//n1 Написать комментарием определение термина "Функции высшего порядка"
+/*
+ "Функции высшего порядка" - это функции которые могут быть в качестве переменных и также могут давать результат,типо симбиоза функции и переменной
+ **/
+
+/*
+ n2 Создать структуру User, задать ему свойства username: String и isPremiumUser: Bool. Создать массив из 5-6 объектов User. С помощью метода filter, отсортировать и вернуть новый массив, состоящий только из премиум-
+ пользователей. Затем, вывести имя каждого премиум-пользователя в консоль.
+ **/
+
+//struct User{
+//    var username: String
+//    var isPremiumUser: Bool
+//    
+//}
+//
+//var user1 = User(username: "BEka", isPremiumUser: true)
+//var user2 = User(username: "Yan", isPremiumUser: false)
+//var user3 = User(username: "Tilek", isPremiumUser: false)
+//var user4 = User(username: "Nooruz", isPremiumUser: true)
+//var user5 = User(username: "Ruslan CHort", isPremiumUser: false)
+//var user6 = User(username: "Kirill", isPremiumUser: true)
+//
+//var usersArray = [user1,user2,user3,user4,user5,user6]
+//
+//usersArray.filter({ a in
+// 
+//    if a.isPremiumUser == true{
+//        print(a.username)
+//        return true
+//    }
+//    else{
+//        return false
+//    }
+//   
+//        
+//}
+
+//n3
+
+//var numArray = [1,2,3,4,5,6,7,8,9,10]
+//var num2 = numArray.filter({
+//   return $0 % 2 != 0
+//    
+//    
+//})
+//print(num2.reduce(5, {
+//    ($0 + $1)
+//}))
 
